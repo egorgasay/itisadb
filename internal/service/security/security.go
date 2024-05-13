8083608823ca0242ed2d53017d06921dad288075
Egor <egorpoletaykin@gmail.com>
2024-05-13 11:34:06 +0300

    :sparkles: ecnryption + fixes

--- internal/service/security/security.go
+++ internal/service/security/security.go
@@ -46,43 +46,59 @@ func (l *SecurityService) HasPermission(claimsOpt gost.Option[models.UserClaims]
 }
 
 func (l *SecurityService) Encrypt(val string) (string, error) {
-    key := []byte(l.encryptionConfig.Key) // Assuming the key is stored in the SecurityConfig and is of appropriate length
+    // Преобразуем ключ из конфигурации шифрования в байты
+    key := []byte(l.encryptionConfig.Key) 
+    // Создаем шифр на основе ключа
     block, err := aes.NewCipher(key)
     if err != nil {
         return "", err
     }
 
+    // Преобразуем строку для шифрования в байты
     plaintext := []byte(val)
+    // Создаем массив для шифротекста с дополнительным местом для IV
     ciphertext := make([]byte, aes.BlockSize+len(plaintext))
-    iv := ciphertext[:aes.BlockSize] // using the first block as IV
+    // Используем первый блок массива в качестве IV
+    iv := ciphertext[:aes.BlockSize] 
+    // Заполняем IV случайными данными
     if _, err := io.ReadFull(rand.Reader, iv); err != nil {
         return "", err
     }
 
+    // Создаем потоковый шифр для шифрования
     stream := cipher.NewCFBEncrypter(block, iv)
+    // Применяем XOR между ключом и текстом, начиная с позиции после IV
     stream.XORKeyStream(ciphertext[aes.BlockSize:], plaintext)
 
+    // Возвращаем зашифрованное значение
     return val, nil
 }
 
 func (l *SecurityService) Decrypt(val string) (string, error) {
+    // Преобразуем ключ из конфигурации шифрования в байты
     key := []byte(l.encryptionConfig.Key) 
 
+    // Создаем шифр на основе ключа
     block, err := aes.NewCipher(key)
     if err != nil {
         return "", err
     }
 
+    // Проверяем, достаточно ли длинный шифротекст
     if len(val) < aes.BlockSize {
         return "", errors.New("ciphertext too short")
     }
 
+    // Используем первый блок шифротекста в качестве IV
     iv := val[:aes.BlockSize]
     val = val[aes.BlockSize:]
 
+    // Создаем потоковый шифр для дешифрования
     stream := cipher.NewCFBDecrypter(block, []byte(iv))
+    // Применяем XOR между ключом и текстом
     stream.XORKeyStream([]byte(val), []byte(val))
 
+    // Возвращаем дешифрованное значение
     return string(val), nil
 }
 
